55a5413ace2a7e1d94dc2491613da9c39912a1bc
eldpswp99 <nrg1392@naver.com>
2020-01-20 15:04:59 +0000

    Updates

---
+++ precache-manifest.9dffea22dfaac7c92f414a116df6acb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758139978ade7a476e272762e43cce06",
+    "url": "/MapViewApp/index.html"
+  },
+  {
+    "revision": "22272a608dac4763a3d9",
+    "url": "/MapViewApp/static/css/main.314f0d5f.chunk.css"
+  },
+  {
+    "revision": "32df0c4b7000ea2c33b2",
+    "url": "/MapViewApp/static/js/2.d4c4ee8e.chunk.js"
+  },
+  {
+    "revision": "308894023d852c562fa319f67551e273",
+    "url": "/MapViewApp/static/js/2.d4c4ee8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22272a608dac4763a3d9",
+    "url": "/MapViewApp/static/js/main.b1d67bdc.chunk.js"
+  },
+  {
+    "revision": "39f48d0b58502f044628",
+    "url": "/MapViewApp/static/js/runtime-main.18934a82.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MapViewApp/precache-manifest.fbcb83df9bc2d76b2a2ffefe5dce1076.js"
+  "/MapViewApp/precache-manifest.9dffea22dfaac7c92f414a116df6acb3.js"
 );
 
 self.addEventListener('message', (event) => {
